98b8397df2120e13b96442f21b82eca933f90c03
Lai Shu Xian <sxlai2@illinois.edu>
2020-12-07 06:28:44 -0600

    p3 binary tree all paths

---
+++ fall2014p3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "vector.h"
+#include "tree.h"
+
+// FUNCTION DEFINITIONS
+void findPathRecursive(node * root, int expectedSum, int currentSum, vector * path);
+void findPath(node * root, int expectedSum);
+
+
+// Don't change the main function
+int main(int argc, char * argv[])
+{
+    if (argc < 1)
+    {
+        printf("Not enough arguments\n");
+        return -1;
+    }
+
+    node * root = create_tree(argv[1]);
+    int sum;
+    printf("Please enter a sum: ");
+    scanf("%d", &sum);
+
+    findPath(root, sum);
+    delete_tree(root);
+}
+
+// Recursive helper function for printing paths within a tree
+// that add up to an expected sum
+void findPathRecursive(node * root, int expectedSum, int currentSum, vector * path)
+{
+    // YOUR CODE HERE
+    if (currentSum == expectedSum){
+        int end = path->back;
+        for (int i = 0; i<=end; i++){
+            printf("%d ", path[i]);
+        }
+        printf("\n");
+        return;
+    }
+    if (!root){
+        return;
+    }
+    if ((currentSum + (root->value)<= expectedSum) &&(!isFull(path)){
+        currentSum += root->value;
+        pushBack(path, root->value);
+        findPathRecursive(root->left, expectedSum, currentSum, path);
+        findPathRecursive(root->right, expectedSum, currentSum, path);
+    }
+    //findPathRecursive(root->left, expectedSum, currentSum, path);
+    //findPathRecursive(root->right, expectedSum, currentSum, path);
+
+}
+
+// Finds and prints all paths within a tree that add to an expected sum
+void findPath(node * root, int expectedSum)
+{
+    // YOUR CODE HERE
+    int currSum;
+    vector* myvector;
+    currSum = 0;
+    vectorInit(myvector);
+    findPathRecursive(root, expectedSum, currSum, myvector);
+    return;
+}
