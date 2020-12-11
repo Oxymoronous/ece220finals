e82c5971b8be5588be7a9c0a5a4815c0c311d40f
Lai Shu Xian <sxlai2@illinois.edu>
2020-12-11 06:48:28 -0600

    concepts: encapsulation, LC3, BST

---
+++ conceptsq234.c
@@ -0,0 +1,18 @@
+//question 2: encapsulation
+
+1. Private members are 'x', 'y' and 'radius' in the class Circle.
+2. Members are by default private if not declared. 
+3. 'x', 'y' and 'radius' are only accessible and visible within the class Circle.
+
+1. The public members of Circle class are accessible and visible from any function.
+2. Public members can be accessed anywhere by using the address operator '.'
+    in all functions
+3. For example, in function main(), 'Move' and 'Scale' are accessed.
+
+//q3
+3. For a nested subroutine in LC3, register R7 must be caller saved.
+
+//q4
+The tree is not a binary search tree because there are left subtrees that
+have greater values than their parent nodes.
+
